4c506d7b97bddd08700d39fb4942457cd6a7c800
Nafis777777 <115471692+Nafis777777@users.noreply.github.com>
2024-09-23 17:12:38 +0600

    Update info.py

--- info.py
+++ info.py
@@ -21,8 +21,8 @@ PORT = environ.get("PORT", "8080")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/68d28011b2bc356b5db01.png')).split()
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
+PICS = (environ.get('PICS', 'https://ibb.co.com/qnCfz25')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5790252632').split()]
