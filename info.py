5cce3af71517999df4a87f4aec9c851d22a2e043
Nafis777777 <115471692+Nafis777777@users.noreply.github.com>
2024-09-23 22:29:49 +0600

    Update info.py

--- info.py
+++ info.py
@@ -71,6 +71,9 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', '0') #Always use website url from api section 
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '0')
+
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 120))
 SELF_DELETE = environ.get('SELF_DELETE', True)
